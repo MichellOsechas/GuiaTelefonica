9d17727bec13b809c6ce2858e0dadf84837deaca
MichellOsechas <90647320+MichellOsechas@users.noreply.github.com>
2023-10-21 13:31:04 -0400

    Add files via upload

--- eventos.js
+++ eventos.js
@@ -39,6 +39,30 @@ const validateInput = (input, validation) => {
   }
 }
 
+const validaEditteInput = (input, validation, validationEditName, validationEditPhone, btn) => {
+
+  // Verifico las validaciones para quitar el atributo disabled del boton
+  if (validationEditName && validationEditPhone) {
+    btn.disabled = false;
+  } else {
+    btn.disabled = true;
+  }
+
+  // Valido si el input esta vacio
+  // Si la validacion es verdadera
+  // Si la validacion es falsa
+  if (input.value === '') {
+    input.classList.remove('error');
+    input.classList.remove('correct');
+  } else if (validation) {
+    input.classList.add('correct');
+    input.classList.remove('error');
+  } else {
+    input.classList.add('error');
+    input.classList.remove('correct');
+  }
+}
+
 inputName.addEventListener('input', e => {
   nameValidation = NAME_REGEX.test(inputName.value);
   validateInput(inputName, nameValidation)
@@ -63,7 +87,7 @@ form.addEventListener('submit', e => {
   <button class="edit-btn"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="edit-icon">
     <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
   </svg>
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="check-icon check" disabled>
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="check-icon check">
   <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z" />
   </svg></button>
   `;
@@ -89,6 +113,9 @@ list.addEventListener('click', e => {
     const editName = editBtn.parentElement.children[1];
     const editPhone = editBtn.parentElement.children[2];
 
+    let editNameValidation = true;
+    let editPhoneValidation = true;
+
     if (!editBtn.classList.contains('editando')) { //? Contains?
       editBtn.classList.add('editando');
       editName.removeAttribute('readonly');
@@ -101,37 +128,21 @@ list.addEventListener('click', e => {
       editName.classList.add('correct');
       
       editName.addEventListener('input', e => {
-        nameValidation = NAME_REGEX.test(editName.value);
-        validateInput(editName, nameValidation)
-        formBtn.disabled = true;   
+        editNameValidation = NAME_REGEX.test(editName.value);
+        validaEditteInput (editName, editNameValidation, editNameValidation, editPhoneValidation, editBtn)       
       });
       
       editPhone.addEventListener('input', e => {
-        phoneValidation = PHONE_REGEX.test(editPhone.value);
-        validateInput(editPhone, phoneValidation)
-        formBtn.disabled = true;   
+        editPhoneValidation = PHONE_REGEX.test(editPhone.value);
+        validaEditteInput (editPhone, editPhoneValidation, editNameValidation, editPhoneValidation, editBtn)          
       });
-      
-      // nameValidation = false;
-      // phoneValidation = false;
-
-      // const editBtn2 = e.target.closest('.check-icon');
-      // const editBtn3 = editBtn.parentElement.children[4];
-
-      //   // Verifico las validaciones para quitar el atributo disabled del boton
-      //   if (nameValidation && phoneValidation) {
-      //     editBtn2.disabled = false;
-      //   } else {
-      //     editBtn2.disabled = true;
-      //   }
-            
+           
     } else {
       editBtn.classList.remove('editando');
       editName.setAttribute('readonly', true);
       editPhone.setAttribute('readonly', true);
       editPhone.classList.remove('correct');
       editName.classList.remove('correct');
-      
       
       // Guardar valor
       editName.setAttribute('value', editName.value);
